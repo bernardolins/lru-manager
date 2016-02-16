09a187b119a1b179def77d7f656e54149b4bc150
Asomal <victorlamosabauer@gmail.com>
2016-02-16 17:15:08 -0200

    incrementei a main e crei tab.h

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
+#include<time.h>
+#include"tab.h"
 
-#define N 20
+#define NUM_PROC 5
+#define NUM_PAG 10
 
 void *processo(void *arg) {
 	int id = *(int*)arg;
+	int i;
 	printf("Processo[%d] inicializou!\n", id);
+	struct PageTable PT = IniciaTabela(id);
+	printf("Tabela de paginas do processo[%d] = ", id);
+	for(i = 0; i < NUM_PAG; i++) {
+		printf("%d ", PT.TabelaPaginas[i]);
+	}
+	printf("\n");
 	pthread_exit(NULL);
 
 }
@@ -14,16 +24,23 @@ void *processo(void *arg) {
 int main(int argc, char **argv) {
 	int i;
 	int *arg;
-	pthread_t threads[N];
+	pthread_t threads[NUM_PROC];
 
-	for(i = 0; i < N; i++) {
+	for(i = 0; i < NUM_PROC; i++) {
 		arg = malloc(sizeof(int));
 		*arg = i;
-		pthread_create(&threads[i], NULL, processo, (void*)arg);
+		if(pthread_create(&threads[i], NULL, processo, (void*)arg)) {
+			printf("Erro ao criar as threads.\n");
+			exit(-1);
+		}
+		sleep(3);
 	}	
 
-	for(i = 0; i < N; i++) {
-		pthread_join(threads[i], NULL);
+	for(i = 0; i < NUM_PROC; i++) {
+		if(pthread_join(threads[i], NULL)) {
+			printf("Erro no join das threads.\n");
+			exit(-1);
+		}
 	}
 
 	pthread_exit(NULL);
