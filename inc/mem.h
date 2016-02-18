976c95bc2f1e855b597fc513ff86d6e68841a2ee
Bernardo Lins <bernardolins28@gmail.com>
2016-02-18 16:30:32 -0200

    Criando estrutura de memória

--- inc/mem.h
+++ inc/mem.h
@@ -10,16 +10,25 @@
 
 #define TAM_MEM 64
 
+#define NUM_PROC 5
+
 struct FRAME {
   int NumProcesso;
   int Pagina;
 };
 
-struct FRAME* AreaDeMemoria(int nframe);
+struct Memoria {
+  int ListaDeProcessos[NUM_PROC];
+  struct PageTable ListaDePaginas[NUM_PROC];
+};
 
+struct Memoria InicializaMemoria();
+
+struct FRAME* AreaDeMemoria(int nframe);
 bool PaginaNaMemoria(struct PageTable *PT, int paginaEscolhida);
 void AtualizaReferencia(int numPagina, int posicao);
 int EscolhePagina();
 void SolicitaPagina(struct FRAME *areaMemoria, struct PageTable *PT, int id);
 void ImprimeMemoria(struct FRAME *areaMemoria, int tamanho);
+
 #endif
